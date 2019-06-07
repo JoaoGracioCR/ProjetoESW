3dfe5593be8d6076725d08f8456e70c6296abc8e
Vasco Pinho <vpinho@deloitte.pt>
2019-06-07 12:43:23 +0100

    1

--- zServer/src/models/docentes.js
+++ zServer/src/models/docentes.js
@@ -3,8 +3,8 @@ var mongoose= require('mongoose');
 var docenteSchema = new mongoose.Schema({
 	IdDocente:{type:Number, unique: true},
     nomeDocente:{type: String},
-    GabineteDocente:{type: String},
-    HorarioDocente :{type: String}
+    gabineteDocente:{type: String},
+    horarioDocente :{type: String}
 });
 
 var docente = mongoose.model('docentes',autorizacaoSchema);
