a3ab89a32ac7f85d20741c1e6f3550fc49c02d83
Vasco Pinho <vpinho@deloitte.pt>
2019-06-14 17:43:26 +0100

    Editar occorencia

--- zServer/src/routes/ocorrencias.js
+++ zServer/src/routes/ocorrencias.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 var ocorrenciaModel = require('../models/ocorrencia');
-mongoose.connect('mongodb+srv://projeto:projeto@clusteresw-idz8g.mongodb.net/ESW?retryWrites=true',{useNewUrlParser:true});
+mongoose.connect('mongodb+srv://projeto:projeto@clusteresw-idz8g.mongodb.net/ESW?retryWrites=true', { useNewUrlParser: true });
 
 // Listar ocorrencias
 router.get('/', (req, res) => {
@@ -11,24 +11,24 @@ router.get('/', (req, res) => {
             console.log(err);
             res.status(404).send();
         } else {
-            res.status(200).send({ocorrencias});
+            res.status(200).send({ ocorrencias });
         }
     });
 });
 
 router.get('/editar/:id', (req, res) => {
-   // res.send(req.params.id);
-   let ocorrencia = {
-       id: 10,
-       nome: "rui",
-       nestudante:100000000
-   }
-   res.render("editarOcorrencia",{
-       ocorrencia:ocorrencia,
-       user:{
-           name:"Rui"
-       }
-   });
+    // req.params.id
+
+    ocorrenciaModel.findOne({idOcorrencia:req.params.id},(err,result)=>{
+        res.render("editarOcorrencia", {
+            ocorrencia: result,
+            user: {
+                name: "Rui"
+            }
+        });
+    });
+
+    
 });
 
 module.exports = router;
